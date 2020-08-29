a5cb425c14931a1a1efbcac0e19160943f44b0ca
Nawinav <avnaveenkumar16@gmail.com>
2020-08-29 19:38:05 +0530

    Updated database

---
+++ MyShopAug/MyShopAug.Sql/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using MyShopAug.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopAug.Sql
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("MyConnection")
+        {
+
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductCategory> productCategories { get; set; }
+    }
+}
